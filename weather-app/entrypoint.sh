f4d125b0f22d55db3431e73b98a4e6bf9dcd45ac
Grachik Navasardyan <g.navasardyan@equiron.com>
2026-07-06 11:43:12 +0300

    modified:   weather-app/docker-compose.yml 	modified:   weather-app/entrypoint.sh 	modified:   weather-app/nginx.conf.template

--- weather-app/entrypoint.sh
+++ weather-app/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${NVIDIA_API_KEY} ${LLM_BASE_URL}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${VITE_NVIDIA_API_KEY} ${VITE_LLM_ENDPOINT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
